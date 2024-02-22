1997c6224e253959394fd75fedbcc527f23cae29
xavixava <xavier.antunes@tecnico.ulisboa.pt>
2024-02-22 15:51:58 +0000

    First Commit

---
+++ assig1.ino
@@ -0,0 +1,76 @@
+#include <Arduino.h>
+#if defined(ESP32) || defined(ARDUINO_RASPBERRY_PI_PICO_W)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#elif __has_include(<WiFiNINA.h>)
+#include <WiFiNINA.h>
+#elif __has_include(<WiFi101.h>)
+#include <WiFi101.h>
+#elif __has_include(<WiFiS3.h>)
+#include <WiFiS3.h>
+#endif
+
+#include <Firebase_ESP_Client.h>
+#include "pitches.h"
+
+
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+
+// Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
+
+#define WIFI_SSID "WIFI_AP"
+#define WIFI_PASSWORD "WIFI_PASSWORD"
+
+#define API_KEY "API_KEY"
+
+const int trigPin = D3;
+const int echoPin = D5;
+const int buzzerPin = D2;
+
+// defines variables
+long duration;
+int distance;
+float distanceCm;
+
+
+#define SOUND_VELOCITY 0.034
+#define CM_TO_INCH 0.393701
+
+
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(115200); // Starts the serial communication
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  Serial.print("Duration: ");
+  Serial.println(duration);
+
+  tone(buzzerPin, 262);
+  
+  delay(500);
+
+  
+}
